05d1acbd861d77ff0b5b61e9fb04bf18b558309a
Kama <mshamratow@gmail.com>
2024-04-11 15:02:32 +0300

    autoscalemode changed

--- PasswordGenerator/Form1.Designer.cs
+++ PasswordGenerator/Form1.Designer.cs
@@ -178,8 +178,8 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
             ClientSize = new Size(1236, 660);
             Controls.Add(button2);
             Controls.Add(comboBox4);
